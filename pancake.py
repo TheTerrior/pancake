e0a6cdcd6daddf67b32fceb8ca06ec367fa93a35
TheTerrior <werbird10@gmail.com>
2023-03-17 01:39:43 -0700

    Added help menu

--- pancake.py
+++ pancake.py
@@ -16,5 +16,19 @@ def testing():
     print(output)
 
 if __name__ == "__main__":
-    print(sys.argv)
+    args = sys.argv
+    if len(args) <= 1:
+        print("error: no operation specified (use -h for help)")
+    elif "-v" in args or "--version" in args:
+        print("pancake 0.0.1")
+    elif "-h" in args or "--help" in args:
+        print("usage:  pancake <option> [...]\n"
+              + "options:\n"
+              + "\t-h --help\n"
+              + "\t-V --version\n"
+              + "\t-s --install\t[packages]\n"
+              + "\t-r --remove\t[packages]\n"
+              + "\t-u --update\t\n"
+              + "\t-q --query\t<package>\n"
+              )
 
