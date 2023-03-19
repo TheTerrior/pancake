43870d046b736191f660b19aae4651d66fa0dc1f
TheTerrior <werbird10@gmail.com>
2023-03-18 23:19:19 -0700

    Updated method of argument reading

--- pancake.py
+++ pancake.py
@@ -15,20 +15,78 @@ def testing():
 
     print(output)
 
+def help():
+    print("usage:  pancake <option> [...]\n"
+          + "options:\n"
+          + "\t-h --help\n"
+          + "\t-V --version\n"
+          + "\t-s --install\t[packages]\n"
+          + "\t-r --remove\t[packages]\n"
+          + "\t-u --update\t\n"
+          + "\t-q --query\t<package>\n"
+          )
+
+def version():
+    print("pancake 0.0.1")
+
+
+def main():
+    update = False
+    install = False
+    remove = False
+    query = False
+
+    for arg in sys.argv[1:]:
+        match arg:
+            case "-h" | "--help":
+                help()
+                return
+            case "-v" | "--version":
+                version()
+                return
+            case "--update":
+                update = True
+            case "--install":
+                install = True
+            case "--remove":
+                remove = True
+            case "--query":
+                query = True
+            case _:
+                if arg[0] == "-":
+                    if len(arg) == 1:
+                        print("error: invalid syntax")
+                        return
+                    for flag in arg[1:]:
+                        match flag:
+                            case "h":
+                                help()
+                                return
+                            case "v":
+                                version()
+                                return
+                            case "u":
+                                update = True
+                            case "s":
+                                install = True
+                            case "r":
+                                remove = True
+                            case "q":
+                                query = True
+                            case _:
+                                print(f"error: unknown flag '{flag}'")
+                else:
+                    print("error: invalid syntax")
+                    return
+
+    # ensure there are no hanging flags/options
+    # ensure there are no doubled flags/options (like install and remove)
+
+
+
+
+
+        
 if __name__ == "__main__":
-    args = sys.argv
-    if len(args) <= 1:
-        print("error: no operation specified (use -h for help)")
-    elif "-v" in args or "--version" in args:
-        print("pancake 0.0.1")
-    elif "-h" in args or "--help" in args:
-        print("usage:  pancake <option> [...]\n"
-              + "options:\n"
-              + "\t-h --help\n"
-              + "\t-V --version\n"
-              + "\t-s --install\t[packages]\n"
-              + "\t-r --remove\t[packages]\n"
-              + "\t-u --update\t\n"
-              + "\t-q --query\t<package>\n"
-              )
+    main()
 
